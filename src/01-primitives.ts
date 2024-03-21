988fc9d55f1eb0824c578ef51bb203034a734ad6
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-03-21 20:49:53 +0300

    make notes of primitive types

---
+++ src/01-primitives.ts
@@ -0,0 +1,20 @@
+//numbers
+let x = 10;
+let y = 10.5;
+let d = NaN;
+//string 
+let str1 : string;
+str1 = 'Hello world';
+let symb = Symbol('as');
+//boolean 
+let o = true;
+o = false;
+//nothing 
+let h: undefined = undefined;
+let g: null = null;
+//literal
+const num = 100;
+const str2 = 'str';
+//universal
+let mn:any = 1;
+let xx: unknown = 2;
